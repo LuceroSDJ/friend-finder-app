92631388209640eaa8b00e5db440078ef996e66c
Carmen Lucero Sandoval <carmenlucero786@gmail.com>
2019-03-23 13:48:25 -0500

    re-organized files & added updated html files

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,32 +1,36 @@
-/* =========== NOTES =============
+/* =========== NOTES =====================
     Routes must be defined before the server can begin accepting requests.
     The route describes how to handle the path portion of the URL in the HTTP request to the Express server. 
     First part of defining the route: HTTP request method (ex. GET or POST)
     Second part: the path specified in the URL (ex. '/' for the root of the website). (Node.js; Dayley-Second Edition)
 */
 
-// Require dependencies
+// =========== Require/import Dependencies ===============
 // path package sets the correct file path for html files
-var express = require("express");  //test line
+    //var express = require("express");  //test line
 var path = require('path');
-
+//var express = require("express");
 // Set up the Express App
-var app = express();
+//var app = express();
 
 
 // ====== ROUTES ===========
-module.export = function(app) {
+// syntax: app.<method>(path,[middleware or callback], callback)
+// Middleware parameter are 0 or more middleware functions applied before executing the callback function.
+
+module.exports = function(app) {
   // Include a GET Route to /survey which should display the survey page.
-  app.get('/survey', function(req, res) {
-      res.sendFile(path.join(__dirname, '../public/survey.html'));
+  app.get("/survey", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
   
   // Add a default, catch-all route that leads to home.html which displays the home page.
   // app.get('/', function(req, res) {
   //   res.sendFile(path.join(__dirname, '../public/home.html'));
   // });
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/home.html'));
-  });
+  
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  }); 
 
 };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// ================= Require Dependencies =============
+// ================= Require/import Dependencies =============
 var express = require("express");
-var path = require("path");
+var path = require("path"); 
 
 /* ================= NOTES ============================
     In order to implement Express as the HTTP server for this node.js app,
@@ -8,32 +8,55 @@ var path = require("path");
 */
 // Set up the Express App & create the required instance of Express to act as the HTTP server for this application.
 var app = express();
-// Assign localhost PORT
+// Define localhost PORT we want to listen to
 var PORT = process.env.PORT || 3000;
 
-// Set up the Express app to handle data parsing
+//Set up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// ====== test =======
+// ====== test ==============
+/*
 app.get('/', function (req, res) {
     res.send('Hello from Express!')   //✓
 })
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../app/public/survey.html"));
+});  //Not working as expected
+*/
+// ============================
 
-
+/*
     // Include a GET Route to /survey which should display the survey page.
-app.get('/survey', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/survey.html'));
+app.use('/survey', function(req, res) {
+    res.sendFile(path.join(__dirname, 'app/public/survey.html'));
 })
+*/
 
-// router 
-    //require("./app/routing/apiRoutes.js")(app);
-//require("./app/routing/htmlRoutes.js");
-//var survey = require("./app/routing/htmlRoutes")(app);
-//require(path.join(__dirname, './routing/htmlRoutes'))(app);
+// routes
+//var htmlRoutes = require("./app/routing/htmlRoutes")(app);
+//require("./app/routing/htmlRoutes")(app);
+
+
+/* After defining routes in htmlroutes.js, load the router module in the app (server.js)
+    example: var birds = require('./birds')
+    ...
+    app.use('/birds', birds)
+    var survey = require('app/public/survey.html');
+    app.use('app/public/survey.html', survey);
+   */
+
+
+// router (requiered route as demonstrated in class activities)
+//require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
 // ====== ERROR ========
 //' CANNOT FIND MODULE'/'REQUIRE IS NOT A FUNCITON' : try creating a variable = require() => gave me another error
     //var friends = require("./app/data/friends.js");   //checar esta line ?
+
+// ====== ERROR ========
+// Error: ENOENT: no such file or directory, stat '/Users/shanmugasundarammanickam/Desktop/REPOS/friend-finder-app/app/public/survey.html'
 
 /* ============ NOTES ===================
     'The app.listen(port) call binds the inderlying HTTP connection 
