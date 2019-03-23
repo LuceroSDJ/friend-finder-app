ced1a29910f4712cbb8a523eaa390cf8ededcee6
Carmen Lucero Sandoval <carmenlucero786@gmail.com>
2019-03-22 20:14:26 -0500

    added notes

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-// Require dependencies
+// ================= Require Dependencies =============
 var express = require("express");
 var path = require("path");
 
-// Set up the Express App & create an instance of express to act as the HTTP server for this application.
+/* ================= NOTES ============================
+    In order to implement Express as the HTTP server for this node.js app,
+    we must create an instance of Express and begin listening on a port (add app.listen(port) at the end of this file)
+*/
+// Set up the Express App & create the required instance of Express to act as the HTTP server for this application.
 var app = express();
+// Assign localhost PORT
 var PORT = process.env.PORT || 3000;
 
 // Set up the Express app to handle data parsing
@@ -12,7 +17,7 @@ app.use(express.json());
 
 // ====== test =======
 app.get('/', function (req, res) {
-    res.send('Hello World')   //✓
+    res.send('Hello from Express!')   //✓
 })
 
 
@@ -30,8 +35,11 @@ app.get('/survey', function(req, res) {
 //' CANNOT FIND MODULE'/'REQUIRE IS NOT A FUNCITON' : try creating a variable = require() => gave me another error
     //var friends = require("./app/data/friends.js");   //checar esta line ?
 
+/* ============ NOTES ===================
+    'The app.listen(port) call binds the inderlying HTTP connection 
+    to the port and begins listening on it. 
+*/
 
-// listener 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);  //app listening ✓
 });
