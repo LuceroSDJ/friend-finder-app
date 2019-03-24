001a5f981a846a50d9fc3e3839037870b8fe27f5
Carmen Lucero Sandoval <carmenlucero786@gmail.com>
2019-03-24 14:00:04 -0500

    updated files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,17 +6,47 @@ module.exports = function(app) {
 
     // Add GET route with url /api/friends. This will be used to display JSON of all possible friends.
     app.get("/api/friends", function(req, res) {
-        res.json(friendsData);  // I am not getting my response as expected 
+        res.json(friendsData);  // I had to changed home route in order for this api route to work
     });
-
-    // Add POST routes /api/friends to handle incoming survey results.
-    // This route will also be used to handle the compatibility logic.
+/*  ================== POST Instructions ==============================
+    Add POST routes /api/friends to handle incoming survey results.
+    This route will also be used to handle the compatibility logic.
+    =================================================================== */
     app.post("/api/friends", function(req, res) {
-        //res.json(friendsData);  // I am not getting my response as expected 
+        //res.json(friendsData); 
 
-        if (friendsData) {
-            friendsData.push(req.body);
-            //res.json(true);
+        // access user's input values like a regular object with dot notation 
+        var userInput = req.body;
+        console.log("user's answers (req.body) = " + userInput);   // terminal => user's answers (req.body) = [object Object]
+        var inputScores = userInput.Scores;
+        console.log(inputScores);   //terminal => [ '1', '1', '1', '1', '1', '1', '1', '1', '1', '1' ] (test)
+        var userName = userInput.Name;  
+        console.log(userName);    // works as expected
+    
+
+    /* compare the difference between current user's scores against those from other users, 
+    question by question. Add up the differences to calculate the totalDifference. 
+            Example:
+            User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+            User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+            Total Difference: 2 + 1 + 2 = 5
+    */
+
+        //return the absolute value of a number abs()method
+        // loop through friends array
+        for(var i = 0; i < friendsData.length; i++) {
+            // find total score of user's input scores
+            // while looping through friends array, find the total amount of default friends
+            // then, compare the differences and figure out how to find the closest to user's input
         }
+
+
+
+        // push the users input to friends array
+        if (friendsData) {
+            friendsData.push(userInput);  //req.body = user's input values
+            //res.json(true);
+        };
+
     });
 };
