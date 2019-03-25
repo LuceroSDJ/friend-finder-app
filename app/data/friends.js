4dbd890f03302b27964eb6f5d5fa2970af65d9bf
Carmen Lucero Sandoval <carmenlucero786@gmail.com>
2019-03-25 01:38:02 -0500

    added image links

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,13 +3,13 @@
 var friends = [
     {
         Name: "Shakira",
-        Photo: "Test",
-        Scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"] //need to find the sum
+        Photo: "https://www.pexels.com/photo/smiling-woman-looking-upright-standing-against-yellow-wall-1536619/",
+        Scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1] //need to find the sum
     },
     {
-        Name: "Bruno Mars",
-        Photo: "Test",
-        Scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+        Name: "Bruno",
+        Photo: "https://www.pexels.com/photo/man-sitting-on-floor-1205033/",
+        Scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     }
 ];
 
