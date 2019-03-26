aadb985e82414712fdb823d1ee6446ad49712707
Carmen Lucero Sandoval <carmenlucero786@gmail.com>
2019-03-25 23:37:47 -0500

    changed pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,13 +3,17 @@
 var friends = [
     {
         Name: "Shakira",
-        Photo: "images/test.jpeg",      
-        // ^======Error: test.jpeg:1 GET http://localhost:3000/assets/images/test.jpeg 404 (Not Found)
+        Photo: "images/shakira.jpeg",      
         Scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1] //need to find the sum
     },
     {
+        Name: "Lana",
+        Photo: "images/lana.jpeg",      
+        Scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3] //need to find the sum
+    },
+    {
         Name: "Bruno",
-        Photo: "images/testImg.jpeg",
+        Photo: "images/bruno.jpeg",
         Scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     }
 ];
